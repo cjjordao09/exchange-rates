3a8fde30428908434a3adb38c1233bfdfc423959
cjjordao <cjjordao09@gmail.com>
2022-05-20 23:22:33 +0200

    Delete exchange-rates directory

--- exchange-rates/src/main/java/com/fnb/exchangerates/exception/RateNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.fnb.exchangerates.exception;
-
-public class RateNotFoundException extends RuntimeException {
-
-    public RateNotFoundException(Long id){
-        super("Could not find rate with ID: " + id);
-    }
-
-}
--- exchange-rates/src/main/java/com/fnb/exchangerates/repository/CurrencyRateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fnb.exchangerates.repository;
-
-import com.fnb.exchangerates.domain.CurrencyRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyRateRepository extends JpaRepository<CurrencyRate, Long> {
-
-}
